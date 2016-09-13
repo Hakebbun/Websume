675c94e98778b810dd966b02b4c92813a255967a
Kevin Hugh <KHugh@inside-76-127.wireless.utoronto.ca>
2016-09-13 17:39:02 -0400

    Publishable 1

--- WebsumeProject/javascript/contact.js
+++ WebsumeProject/javascript/contact.js
@@ -1,8 +1,8 @@
 function sendMail(){
   var form = document.getElementById('emailForm');
   var mailFrom = form.elements['from'].value;
-  var mailReturn = form.elements['return'];
+  var mailReturn = ", ".concat(form.elements['return'].value);
   var mailMessage = form.elements['message'].value;
-  var mail = 'mailto:kevin.hugh93@gmail.com?subject='.concat(mailFrom).concat('&body=').concat(mailMessage);
+  var mail = 'mailto:kevin.hugh93@gmail.com?subject='.concat(mailFrom).concat(mailReturn).concat('&body=').concat(mailMessage);
   window.location.href = mail;
 }
